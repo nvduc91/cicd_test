cf12860e874b64cf3043b4de11adc9944f744ce6
ducnv <duc.nguyen@dzsi.com>
2020-11-10 10:13:48 +0700

    push code

---
+++ src/main/java/com/cicd/demo/TestController.java
@@ -0,0 +1,16 @@
+package com.cicd.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class TestController {
+
+    @GetMapping("{text}")
+    public String testFunction(@PathVariable String text) {
+        return text;
+    }
+}
---
+++ src/test/java/com/cicd/demo/TestControllerTest.java
@@ -0,0 +1,24 @@
+package com.cicd.demo;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TestControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void greetingShouldReturnDefaultMessage(){
+        Assertions.assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/Hello, World",
+                String.class)).contains("Hello, World");
+    }
+}
